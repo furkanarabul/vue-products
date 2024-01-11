f0b4137be979dc0cc6531409ad233d70bc50900b
furkan.arabul <furkan.arabul@internetstores.com>
2024-01-11 06:01:28 +0100

    add navigation arrow to slider

--- src/components/ProductImages.vue
+++ src/components/ProductImages.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-import { Swiper, SwiperSlide } from 'swiper/vue';
-import { Pagination } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/pagination';
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Pagination, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import 'swiper/css/navigation';
 
-const modules = [Pagination];
 
-import { defineProps } from 'vue';
+const modules = [Pagination, Navigation];
+
+import { defineProps } from "vue";
 const props = defineProps({
   selectedColorImages: {
     type: Array,
@@ -21,12 +23,13 @@ console.log(props.selectedColorImages);
     <swiper
       :modules="modules"
       :loop="true"
+      :navigation="true"
       :pagination="{ clickable: true }"
       :breakpoints="{
         768: { slidesPerView: 1 },
         1024: { slidesPerView: 2 },
         1920: { slidesPerView: 3 },
-        2560: { slidesPerView: 4 }
+        2560: { slidesPerView: 4 },
       }"
     >
       <swiper-slide v-for="photo in selectedColorImages" :key="photo.id">
@@ -37,3 +40,12 @@ console.log(props.selectedColorImages);
   </div>
 </template>
 
+<style>
+.swiper-button-next,
+.swiper-container-rtl .swiper-button-prev,
+.swiper-button-prev, 
+.swiper-container-rtl .swiper-button-next{
+    color: black;
+    opacity: 0.5;
+}
+</style>
